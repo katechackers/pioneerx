ac899b6c47a43b1716365f6798f7a7d02648ca7c
Wonbin Lee <wblee800@gmail.com>
2025-03-28 16:00:37 -0600

    Set Fastify: add dotenv support, register autoload plugin, and set loger true

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,8 +1,12 @@
+import "dotenv-safe/config";
+
 import { makeApp } from "./makeApp";
 
-const Host = process.env.HOST ?? "0.0.0.0";
-const Port = Number(process.env.PORT ?? "7000");
+const HOST = process.env.HOST ?? "0.0.0.0";
+const PORT = Number(process.env.PORT ?? "7000");
 
 const app = makeApp();
 
-await app.listen({ host: Host, port: Port });
+await app.listen({ host: HOST, port: PORT });
+
+app.log.info({}, `Server listening at http://localhost:${PORT}`);
---
+++ app/src/plugins/cors.ts
@@ -0,0 +1,11 @@
+import fp from "fastify-plugin";
+import fastifyCors from "@fastify/cors";
+
+export default fp(
+  async (app) => {
+    await app.register(fastifyCors, {
+      preflightContinue: true,
+    });
+  },
+  { name: "cors" },
+);
